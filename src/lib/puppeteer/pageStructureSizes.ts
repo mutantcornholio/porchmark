c9efee31eccad8ca062e590a6fc0d3b8da49cfb4
alekzonder <alekzonder@gmail.com>
2021-06-02 11:29:09 +0700

    draft for test

--- src/lib/puppeteer/pageStructureSizes.ts
+++ src/lib/puppeteer/pageStructureSizes.ts
@@ -3,27 +3,31 @@ import puppeteer from 'puppeteer';
 
 export const getPageStructureSizes = (page: puppeteer.Page): Promise<IPageStructureSizes> => {
     return page.evaluate(() => {
-        const scripts = Array.from(document.querySelectorAll('script'));
+        // const scripts = Array.from(document.querySelectorAll('script'));
 
-        const scriptsByType = scripts.reduce((acc, element) => {
-            (acc[element.type] = acc[element.type] || []).push(element);
-            return acc;
-        }, {} as {[index: string]: HTMLScriptElement[]});
+        // const scriptsByType = scripts.reduce((acc, element) => {
+        //     (acc[element.type] = acc[element.type] || []).push(element);
+        //     return acc;
+        // }, {} as {[index: string]: HTMLScriptElement[]});
 
         return {
+            // bytes: 0,
             bytes: new Blob(Array.from(document.documentElement.outerHTML)).size,
-            script: new Blob(
-                scripts.map((e) => e.outerHTML),
-            ).size,
-            style: new Blob(
-                Array.from(document.querySelectorAll('style')).map((e) => e.outerHTML),
-            ).size,
-            scripts: Object.entries(scriptsByType).reduce((acc, [scriptType, _elements]) => {
-                acc[scriptType] = new Blob(
-                    _elements.map((element) => element.outerHTML),
-                ).size;
-                return acc;
-            }, {} as {[index: string]: number}),
+            // script: new Blob(
+            //     scripts.map((e) => e.outerHTML),
+            // ).size,
+            script: 0,
+            style: 0,
+            // style: new Blob(
+            //     Array.from(document.querySelectorAll('style')).map((e) => e.outerHTML),
+            // ).size,
+            // scripts: Object.entries(scriptsByType).reduce((acc, [scriptType, _elements]) => {
+            //     acc[scriptType] = new Blob(
+            //         _elements.map((element) => element.outerHTML),
+            //     ).size;
+            //     return acc;
+            // }, {} as {[index: string]: number}),
+            scripts: {},
         };
     });
 };
